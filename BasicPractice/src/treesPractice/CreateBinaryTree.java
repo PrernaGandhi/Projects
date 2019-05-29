c0b1d66d2ffeea9631fdc0758de20299a8a3f0a3
Prerna Gandhi <prernagandhi97@gmail.com>
2019-05-29 23:08:56 +0530

    left view right view

--- BasicPractice/src/treesPractice/CreateBinaryTree.java
+++ BasicPractice/src/treesPractice/CreateBinaryTree.java
@@ -1,5 +1,10 @@
 package treesPractice;
 
+import java.util.Map.Entry;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.Vector;
+
 class CreateBinaryTree {
 	
 	Node root;
@@ -7,7 +12,7 @@ class CreateBinaryTree {
 	class Node{
 		int key;
 		Node left,right;
-		 Node(int key){
+		Node(int key){
 			 this.key = key;
 			 left = null;
 			 right = null;
@@ -23,13 +28,130 @@ class CreateBinaryTree {
 		inorder(n.right);
 	}
 	
+	void iterativePreOrder(Node n){
+		if(n == null){
+			return;
+		}
+		Stack<Node> stack = new Stack<>();
+		stack.push(n);
+		while(!stack.isEmpty()){
+			Node val = stack.pop();
+			System.out.print(val.key + " ");
+			if(val.right != null){
+				stack.push(val.right);
+			}
+			if(val.left != null){
+				stack.push(val.left);
+			}
+		}
+	}
+	
+	void topView(Node n){
+		
+	}
+	
+	void printVerticalOrderTraversal(Node n){
+		TreeMap<Integer,Vector<Integer>>m = new TreeMap<>();
+		int hd = 0;
+		getVerticalOrder(n,hd,m);
+		for(Entry<Integer,Vector<Integer>> entry : m.entrySet()){
+			System.out.println(entry.getValue());
+		}
+		
+	}
+	
+	void postOrderWithOneStack(Node n){ 
+		Stack<Node> stack = new Stack<>(); 
+	    while(true){
+	        while(n != null){ 
+	            stack.push(n); 
+	            stack.push(n); 
+	            n = n.left; 
+	        } 
+	        if(stack.empty()){
+	        	return; 
+	        }
+	        n = stack.pop(); 
+	          
+	        if(!stack.empty() && stack.peek() == n){
+	        	n = n.right; 
+	        }else {      
+	            System.out.print(n.key + " "); 
+	            n = null; 
+	        } 
+	    } 
+    }
+	
+	void leftView(Node n){
+		Stack<Node> s = new Stack<>();
+		Node temp = n;
+		while(temp != null){
+			s.push(temp);
+			temp = temp.left;
+		}
+		while(!s.isEmpty()){
+			System.out.print(s.pop().key + " ");
+		}
+	}
+	
+	void rightView(Node n){
+		Stack<Node> s = new Stack<>();
+		Node temp = n;
+		while(temp != null){
+			s.push(temp);
+			temp = temp.right;
+		}
+		while(!s.isEmpty()){
+			System.out.print(s.pop().key + " ");
+		}
+	}
+	
+	private void getVerticalOrder(Node n, int hd,
+			TreeMap<Integer, Vector<Integer>> m) {
+		if(n == null){
+			return;
+		}
+		Vector<Integer> k = m.get(hd);
+		if(k == null){
+			k = new Vector<>();
+			k.add(n.key);
+		}else{
+			k.add(n.key);
+		}
+		m.put(hd, k);
+		getVerticalOrder(n.left, hd - 1, m);
+		getVerticalOrder(n.right, hd + 1, m);
+	}
+
 	void preorder(Node n){
 		if(n == null){
 			return;
 		}
 		System.out.print(n.key + " ");
-		inorder(n.left);
-		inorder(n.right);
+		preorder(n.left);
+		preorder(n.right);
+	}
+	
+	void iterativePostOrderWithTwoStacks(Node n){
+		if(n == null){
+			return;
+		}
+		Stack<Node> stack1 = new Stack<>();
+		Stack<Node> stack2 = new Stack<>();
+		stack1.push(n);
+		while(!stack1.isEmpty()){
+			Node val = stack1.pop();
+			stack2.push(val);
+			if(val.left != null){
+				stack1.push(val.left);
+			}
+			if(val.right != null){
+				stack1.push(val.right);
+			}
+		}
+		while(!stack2.isEmpty()){
+			System.out.print(stack2.pop().key + " ");
+		}
 	}
 	
 	void postorder(Node n){
@@ -70,11 +192,42 @@ class CreateBinaryTree {
 		System.out.println("Inorder :: ");
 		cbt.inorder(root);
 		System.out.println();
+		System.out.println("Iterative Postorder with 2 stacks:: ");
+		cbt.iterativePostOrderWithTwoStacks(root);
+		System.out.println();
+		System.out.println("Iterative Postorder without stack ad recursion:: ");
+		TraversalWithoutStackAndRecursion t = new TraversalWithoutStackAndRecursion();
+		TraversalWithoutStackAndRecursion.node root1 = t.newNode(8); 
+		root1.left = t.newNode(3); 
+	    root1.right = t.newNode(10); 
+	    root1.left.left = t.newNode(1); 
+	    root1.left.right = t.newNode(6); 
+	    root1.left.right.left = t.newNode(4); 
+	    root1.left.right.right = t.newNode(7); 
+	    root1.right.right = t.newNode(14); 
+	    root1.right.right.left = t.newNode(13);
+		t.postorderWithoutStackAndRecursion(root1);
+		System.out.println();
 		System.out.println("Postorder :: ");
 		cbt.postorder(root);
 		System.out.println();
+		System.out.println("Postorder with one stack :: ");
+		cbt.postOrderWithOneStack(root);
+		System.out.println();
 		System.out.println("PreOrder :: ");
 		cbt.preorder(root);
+		System.out.println();
+		System.out.println("Iterative PreOrder :: ");
+		cbt.iterativePreOrder(root);
+		System.out.println();
+		System.out.println("Print vertical order traversal : :");
+		cbt.printVerticalOrderTraversal(root);
+		System.out.println();
+		System.out.println("left view ::");
+		cbt.leftView(root);
+		System.out.println();
+		System.out.println("right view ::");
+		cbt.rightView(root);
 	}
 
 }
---
+++ BasicPractice/src/treesPractice/TraversalWithoutStackAndRecursion.java
@@ -0,0 +1,36 @@
+package treesPractice;
+
+public class TraversalWithoutStackAndRecursion {
+
+	class node{
+		int key;
+		node left;
+		node right;
+		boolean visited;
+	}
+	
+	node root;
+	node newNode(int data) { 
+	    node node = new node(); 
+	    node.key = data; 
+	    node.left = null; 
+	    node.right = null; 
+	    node.visited = false; 
+	    return (node); 
+	}
+	void postorderWithoutStackAndRecursion(node h){
+		node temp = h;
+		while(temp != null && temp.visited == false){
+			if(temp.left != null && temp.left.visited == false){
+				temp = temp.left;
+			}else if(temp.right != null && temp.right.visited == false){
+				temp = temp.right;
+			}else{
+				System.out.print(temp.key + " ");
+				temp.visited = true;
+				temp = h;
+			}
+		}
+	}
+	
+}
